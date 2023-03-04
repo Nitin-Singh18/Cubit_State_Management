98af0ed835dadaa8edfd91da0ce90d9abef12c3a
Nitin Singh <i.nitinsingh18@gmail.com>
2023-03-04 16:40:47 +0530

    Cubit Example

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math show Random;
 
 void main() {
   runApp(const MyApp());
@@ -20,11 +22,75 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+const name = ['Foo', 'Bar', 'Baz'];
+
+extension RandomElement<T> on Iterable<T> {
+  T getRandomElement() => elementAt(math.Random().nextInt(length));
+}
+
+class NamesCubit extends Cubit<String?> {
+  //we don't have an initial state therefore we are passing null
+  NamesCubit() : super(null);
+
+  void pickRandomName() => emit(name.getRandomElement());
+}
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final NamesCubit cubit;
+
+  @override
+  void initState() {
+    super.initState();
+    cubit = NamesCubit();
+  }
+
+  @override
+  void dispose() {
+    cubit.close();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+      body: StreamBuilder(
+        stream: cubit.stream,
+        builder: (context, snapshot) {
+          final button = Center(
+            child: TextButton(
+                onPressed: () => cubit.pickRandomName(),
+                child: const Text("Random Name")),
+          );
+
+          //here we are showing different widget based on the current
+          //state of the snapshot
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return button;
+            case ConnectionState.waiting:
+              return button;
+            case ConnectionState.active:
+              return Column(
+                children: [
+                  Text(snapshot.data ?? ''),
+                  button,
+                ],
+              );
+            case ConnectionState.done:
+              return const SizedBox();
+          }
+        },
+      ),
+    );
   }
 }
